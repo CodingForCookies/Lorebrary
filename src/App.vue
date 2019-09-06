c82c34614b1b95934401cce5c5d1a96f4455e44b
Trevin Miller <stumblinbear@gmail.com>
2019-09-06 03:32:59 -0500

    Add changelog dialog

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,58 @@
 <template>
-  <div id="main-loader" v-if="$store.state.startup.is">
+  <v-app v-if="acknowledged != version" style="background-color:#37474f">
+    <v-dialog persistent :value="true" width="500">
+      <v-card>
+        <v-card-title
+          class="pb-0 headline"
+          primary-title>
+          {{ changelog.title }}
+        </v-card-title>
+        <div v-if="changelog.subtitle"
+            class="mx-6 overline">
+          {{ changelog.subtitle }}
+        </div>
+
+        <v-card-text v-if="changelog.text"
+            class="mt-4 py-0">
+          <p v-for="(paragraph, i) in changelog.text" :key="'text-' + i" v-html="paragraph"></p>
+        </v-card-text>
+
+        <v-card-text v-if="changelog.changes"
+            class="mt-4 py-0">
+          <strong>Changes</strong>
+
+          <div v-for="(change, i) in changelog.changes.additions" :key="'additions-' + i"
+            class="success--text">+ {{ change }}</div>
+          <div v-for="(change, i) in changelog.changes.subtractions" :key="'subtractions-' + i"
+            class="error--text">- {{ change }}</div>
+        </v-card-text>
+
+        <v-card-text v-if="changelog.bugs"
+            class="mt-4 py-0">
+          
+          <strong>Bugs</strong>
+
+          <div v-for="(change, i) in changelog.bugs.fixed" :key="'fixed-' + i"
+            class="success--text">- {{ change }}</div>
+          <div v-for="(change, i) in changelog.bugs.added" :key="'added-' + i"
+            class="error--text">+ {{ change }}</div>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn
+            color="primary"
+            text
+            @click="acknowledgeVersion">
+            Yay!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+  <div id="main-loader" v-else-if="$store.state.startup.is">
     <span class="loader"><span class="loader-inner"></span></span>
     <div id="text">
       <span v-for="(stage, i) in $store.state.startup.stages" :key="i"
@@ -15,6 +68,23 @@
 
 <script>
   export default {
+    data: () => ({
+      acknowledged: localStorage.getItem("acknowledged"),
+      version: '0.0.1',
+      changelog: null
+    }),
+    methods: {
+      acknowledgeVersion() {
+        localStorage.setItem("acknowledged", this.version);
+        
+        this.acknowledged = this.version;
+      }
+    },
+
+    created() {
+      this.changelog = require('./changelogs/' + this.version + '.json');
+    },
+
     mounted() {
       // If we're running in an iframe, don't automatically start up. Allow the parent to init startup.
       
